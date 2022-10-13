1066fc84b4d8d06f82691d597a5314a356119ec7
Jianbin Chang <shjwudp@gmail.com>
2022-10-13 21:23:21 +0800

    feat: add generate-server, streamlit-server and python requirements

---
+++ generate-server.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2022 Jianbin Chang
+
+import traceback
+import time
+import argparse
+
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Resource, Api
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+class GPTGenerate(Resource):
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def post(self):
+        start_time = time.time()
+        try:
+            request_j = request.get_json()
+            context = request_j["context"]
+            del request_j["context"]
+
+            input_ids = self.tokenizer(context, return_tensors="pt").input_ids.to(self.model.device)
+            request_j["inputs"] = input_ids
+
+            result = self.model.generate(**request_j)
+
+            if request_j.get("return_dict_in_generate", False):
+                sequences = result["sequences"]
+                sequences = self.tokenizer.batch_decode(
+                    sequences, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+                output = dict(
+                    sequences=sequences,
+                    sequences_scores=result["sequences_scores"].tolist(),
+                    # scores=[x.tolist for x in result["scores"]],
+                    # beam_indices=result["beam_indices"].tolist(),
+                )
+            else:
+                generate_ids = result
+                output = self.tokenizer.batch_decode(
+                    generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+
+            resp = {
+                "compute_time": time.time() - start_time,
+                "context": context,
+                "output": output,
+            }
+            return make_response(jsonify(resp), 200)
+        except:
+            traceback.print_exc()
+            return traceback.format_exc(), 400
+
+
+class GenerateServer(object):
+    def __init__(self, model, tokenizer):
+        self.app = Flask(__name__)
+        api = Api(self.app)
+        api.add_resource(GPTGenerate, '/generate',
+                         resource_class_args=[model, tokenizer])
+
+    def run(self, url, port):
+        self.app.run(url, debug=False, port=port)
+
+
+def load_model(huggingface_model):
+    tokenizer = AutoTokenizer.from_pretrained(huggingface_model)
+    model = AutoModelForCausalLM.from_pretrained(huggingface_model, device_map="auto")
+
+    return model, tokenizer
+
+
+def main():
+    parser = argparse.ArgumentParser("Huggingface Generate Model Service")
+    parser.add_argument("--huggingface_model", required=True)
+    parser.add_argument("--port", default=55556, type=int)
+    args = parser.parse_args()
+
+    model, tokenizer = load_model(args.huggingface_model)
+    generate_server = GenerateServer(model, tokenizer)
+
+    generate_server.run("0.0.0.0", args.port)
+
+
+if __name__ == "__main__":
+    main()
---
+++ streamlit-server.py
@@ -0,0 +1,121 @@
+import requests
+import json
+import argparse
+
+import streamlit as st
+
+
+def main():
+    parser = argparse.ArgumentParser("Streamlit Server")
+    parser.add_argument("--backend", required=True)
+    parser.add_argument("--model_name", default="Language Model")
+    args = parser.parse_args()
+
+    st.set_page_config(  # Alternate names: setup_page, page, layout
+        layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+        initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+        page_title=f"{args.model_name} Playground",  # String or None. Strings get appended with "• Streamlit".
+        page_icon=None,  # String, anything supported by st.image, or None.
+    )
+    st.title(f"{args.model_name} Playground")
+    """This app enables you to interact with large language models in a friendly way!"""
+
+    ex_names = [
+        "In a shocking finding, scientists discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.",
+        "The ancient people of Arcadia achieved oustanding cultural and technological developments. Below we summarise some of the highlights of the Acadian society.",
+        """Tweet: "I hate it when my phone battery dies."
+        Sentiment: Negative
+        ###
+        Tweet: My day has been 👍.
+        Sentiment: Positive
+        ###
+        Tweet: This is the link to the article.
+        Sentiment: Neutral
+        ###
+        Tweet: This new movie started strange but in the end it was awesome.
+        Sentiment:""",
+                """Q: Fetch the departments that have less than five people in it.\n
+        A: SELECT DEPARTMENT, COUNT(WOKRED_ID) as "Number of Workers" FROM Worker GROUP BY DEPARTMENT HAVING COUNT(WORKED_ID) < 5;\n
+        ###\n
+        Q: Show all departments along with the number of people in each department\n
+        A: SELECT DEPARTMENT, COUNT(DEPARTMENT) as "Number of Workers" FROM Worker GROUP BY DEPARTMENT;\n
+        ###\n
+        Q: Show the last record of the Worker table\n
+        A: SELECT * FROM Worker ORDER BY LAST_NAME DESC LIMIT 1;\n
+        ###\n
+        Q: Fetch the three max salaries from the Worker table;\n
+        A:""",
+        "白日依山尽",
+    ]
+    example = st.selectbox("Choose an example prompt from this selector", ex_names)
+
+    inp = st.text_area(
+        "Or write your own prompt here!", example, max_chars=10000, height=150
+    )
+
+    try:
+        rec = ex_names.index(inp)
+    except ValueError:
+        rec = 0
+
+    with st.expander("Generation options..."):
+        max_new_tokens = st.slider(
+            "Max new tokens of the generated texts (in tokens)",
+            min_value=1,
+            max_value=128,
+            value=10,
+            step=1,
+        )
+        n = st.slider(
+            "N",
+            min_value=1,
+            max_value=10,
+            value=3,
+            step=1,
+        )
+        do_sample = st.checkbox("Check in to do sample in generate", True)
+        temp = st.slider(
+            "Choose the temperature (higher - more random, lower - more repetitive). For the code generation or sentence classification promps it's recommended to use a lower value, like 0.35",
+            min_value=0.,
+            max_value=1.5,
+            value=1.0 if rec < 2 else 0.35,
+        )
+        top_p = st.slider(
+            "Choose the top-p of the generated texts",
+            min_value=0.,
+            max_value=1.,
+            value=0.95,
+        )
+
+    response = None
+    with st.form(key="inputs"):
+        submit_button = st.form_submit_button(label="Generate!")
+
+        if submit_button:
+            url = args.backend
+            payload = {
+                "context": inp,
+                "max_new_tokens": max_new_tokens,
+                "temperature": temp,
+                "top_p": top_p,
+                "do_sample": do_sample,
+                "num_beams": n,
+                "num_return_sequences": n,
+            }
+
+            query = requests.request("POST", url, json=payload)
+            try:
+                response = query.json()
+                output = response["output"]
+
+                if isinstance(output, dict):
+                    output = output["sequences"]
+
+                st.write(output)
+                st.text(f"Generation done in {response['compute_time']:.3} s.")
+            except Exception as exception:
+                st.error(f"Request failed, status_code={query.status_code}, reason={query.reason}, content={query.content}")
+
+
+if __name__ == "__main__":
+    main()
